727a802aab7dd269c9830c862a12194916cdc45c
Jose Carlos Rivera <josecarlosrt@noreply.github.com>
2021-02-08 23:42:59 -0600

    New update test

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from "react";
-import SearchMovies from "./SearchMovies"
+import SearchMovies from "./components/SearchMovies"
+import Navbar from './components/Navbar';
 import "./App.css";
 
 class App extends React.Component {
   render() {
     return (
       <div className="container">
-        <h1 className="title">Movie Search</h1>
+        <Navbar />
+        <main style={{marginTop: '64px'}}>
+        <h1 className="title">Search Movies</h1>
         <SearchMovies />
+        </main>
       </div>
     );
   }
---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+import { render } from "@testing-library/react";
+import React from "react";
+import "./Navbar.css"
+
+function Navbar() {
+    return (
+        <header className="header-nav">
+            <nav className="navbar">
+                <div className="logo"><a href="/">Movies Info</a>
+                </div>
+            </nav>
+        </header>
+    )
+   
+}
+
+export default Navbar
