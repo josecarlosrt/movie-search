5267ed970b251cf80a27011476eecd44d6807cf7
Jose Carlos Rivera <josecarlosrt@noreply.github.com>
2021-02-08 23:47:31 -0600

    Color Update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,17 @@
 import { render } from "@testing-library/react";
 import React from "react";
-import "./Navbar.css"
+import "./Navbar.css";
 
 function Navbar() {
-    return (
-        <header className="header-nav">
-            <nav className="navbar">
-                <div className="logo"><a href="/">Movies Info</a>
-                </div>
-            </nav>
-        </header>
-    )
-   
+  return (
+    <header className="header-nav">
+      <nav className="navbar">
+        <div className="logo">
+          <a href="/">Movies Info</a>
+        </div>
+      </nav>
+    </header>
+  );
 }
 
-export default Navbar
+export default Navbar;
